ee040a09aed6537bf3189ff08a9c20fb143d72c0
Fátima Fdez <fatimafdez@github.com>
2023-11-15 12:22:49 +0100

    Update

--- include/rs_aruco_detection/ArucoPosition.h
+++ include/rs_aruco_detection/ArucoPosition.h
@@ -1,6 +1,9 @@
 #pragma once
 #include <ros/ros.h>
-#include <string>  // Include necessary header for std::string
+#include <string>
+#include <cv_bridge/cv_bridge.h>
+#include <opencv2/opencv.hpp>
+#include <iostream>
 
 #define ROS_TOPIC_BUFFER_SIZE 1
 
@@ -11,8 +14,13 @@ namespace catec_suministra_detection {
         ~ArucoMarkerPosition();
         void setPosition(double x, double y, double z);
         void getPosition();
+        void imageCallback(const sensor_msgs::ImageConstPtr& msg);  
+
     private:
         double x, y, z;
+        ros::Subscriber image_sub;
+        cv::Mat current_image;
+
     };
 
 } // namespace catec_suministra_detection
--- src/ArucoPosition.cpp
+++ src/ArucoPosition.cpp
@@ -2,6 +2,8 @@
 
 #include "ros/ros.h"
 #include "std_msgs/String.h"
+#include "sensor_msgs/Image.h"
+#include "image_transport/image_transport.h"
 #include <opencv2/opencv.hpp>
 #include <sstream>
 
@@ -23,7 +25,33 @@ namespace catec_suministra_detection
     // getPosition
     void ArucoMarkerPosition::getPosition() 
     {
-    std::cout << "ArucoMarkerPosition: " << this->x << ", " << this->y << ", " << this->z << std::endl;
+        std::cout << "ArucoMarkerPosition: " << this->x << ", " << this->y << ", " << this->z << std::endl;
     }
+
+    void ArucoMarkerPosition::imageCallback(const sensor_msgs::ImageConstPtr& msg)
+    {
+        try
+        {
+            cv_bridge::CvImageConstPtr cv_ptr;
+            cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+
+            // Process the image using OpenCV
+            cv::Mat image = cv_ptr->image;
+
+            // Display the image
+            cv::imshow("RealSense Image", image);
+            cv::waitKey(1);
+
+
+            // Convert the ROS image message to an OpenCV image
+            //current_image = cv_bridge::toCvShare(msg, "bgr8")->image;
+        }
+        catch (cv_bridge::Exception& e)
+        {
+            ROS_ERROR("cv_bridge exception: %s", e.what());
+        }
+        
+    }
+
 
 } // namespace catec_suministra_detection
