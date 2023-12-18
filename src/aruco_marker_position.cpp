4ddc0676a9bb0481f3d021791c3560fb0b87752d
Fátima Fdez <fatimafdez@github.com>
2023-12-18 12:56:02 +0100

    Commented code

--- src/aruco_marker_position.cpp
+++ src/aruco_marker_position.cpp
@@ -2,18 +2,7 @@
 
 namespace catec_suministra {
 
-ArucoMarkerPosition::ArucoMarkerPosition(ros::NodeHandle& nh) :
-        _nh(nh) // Constructor
-                // LAMBDA - is defined with the sintax [capture list] (function arguments) {body}
-                // Apparently this code store inside the subscriber object what it needs to do, it's more clean
-                // & indicates that we are passing the object by reference, so we are not copying it, we are just
-                // passing the pointer to the object
-                // [&] indicates to capture everything, so we have access to all the variables and methods of the class,
-                // as if we write everything outside the lambda function (ros::NodeHandle& nh) : _nh(nh) -> the _nh(nh)
-                // -> nh is define in the cpp file, which creates the node handler, in the header file, we assign a
-                // specific space for the node handler, and in this line we associate the node handler created in the
-                // cpp to the espace assigned in the header just handles everywhere i dont get it, why they are defined
-                // again? i dont understand this (ros::NodeHandle& nh)
+ArucoMarkerPosition::ArucoMarkerPosition(ros::NodeHandle& nh) : _nh(nh) // Constructor
 {
     ROS_DEBUG("[%s]", __PRETTY_FUNCTION__);
 
@@ -34,7 +23,6 @@ ArucoMarkerPosition::ArucoMarkerPosition(ros::NodeHandle& nh) :
     _nh.param<std::string>("calibration_file", calibration_file, "rs_calibration_parameters.yaml");
     _nh.param<std::string>("markers_length_file", markers_length_file, "markers_length.yaml");
 
-
     std::cout << " Me quiero subscribir a " << rs_image_topic << std::endl; // print to see if it works
 
     readCalibrationParameters(calibration_file);
@@ -52,6 +40,16 @@ ArucoMarkerPosition::ArucoMarkerPosition(ros::NodeHandle& nh) :
             ROS_TOPIC_BUFFER_SIZE,
             [&](const sensor_msgs::ImageConstPtr& image_msg) {
                 ROS_DEBUG("[ArucoMarkerPosition::rs_subscriber]");
+                // LAMBDA - is defined with the sintax [capture list] (function arguments) {body}
+                // Apparently this code store inside the subscriber object what it needs to do, it's more clean
+                // & indicates that we are passing the object by reference, so we are not copying it, we are just
+                // passing the pointer to the object
+                // [&] indicates to capture everything, so we have access to all the variables and methods of the class,
+                // as if we write everything outside the lambda function (ros::NodeHandle& nh) : _nh(nh) -> the _nh(nh)
+                // -> nh is define in the cpp file, which creates the node handler, in the header file, we assign a
+                // specific space for the node handler, and in this line we associate the node handler created in the
+                // cpp to the espace assigned in the header just handles everywhere i dont get it, why they are defined
+                // again? i dont understand this (ros::NodeHandle& nh)
                 ArucoDetections aruco_detections = detectArucosPoses(image_msg);
                 publishDetections(image_msg->header, aruco_detections);
             },
@@ -70,7 +68,10 @@ ArucoMarkerPosition::~ArucoMarkerPosition() // Destructor
     ROS_DEBUG("[%s]", __PRETTY_FUNCTION__);
 }
 
-void ArucoMarkerPosition::readCalibrationParameters(const std::string& calibration_filename)
+void ArucoMarkerPosition::readCalibrationParameters(
+        const std::string&
+                calibration_filename) // this function reads a file with the calibration parameters and store the values
+                                      // in the variables _intrinsic_matrix and _dist_coeffs_vector
 {
     ROS_DEBUG("[%s]", __PRETTY_FUNCTION__);
 
@@ -110,7 +111,10 @@ void ArucoMarkerPosition::readCalibrationParameters(const std::string& calibrati
     ROS_INFO("p1: %f, p2: %f", _dist_coeffs_vector.at<double>(0, 3), _dist_coeffs_vector.at<double>(0, 4));
 }
 
-void ArucoMarkerPosition::readMarkersLength(const std::string& markers_length_filename)
+void ArucoMarkerPosition::readMarkersLength(
+        const std::string&
+                markers_length_filename) // this reads the markers_configuration.yaml file and store the values in the
+                                         // variables _store_marker_ids, _store_marker_lengths and _store_marker_frames
 {
     ROS_DEBUG("[%s]", __PRETTY_FUNCTION__);
 
@@ -149,7 +153,7 @@ ArucoDetections ArucoMarkerPosition::detectArucosPoses(const sensor_msgs::ImageC
         int                                   freq, vector_position = 0;
         double                                theta, s;
         cv::Mat                               output_image;
-        cv::Vec3d                             axis, rotation_vector, translation_vector;
+        cv::Vec3d                             axis, rotation_vector;
         cv::Vec4d                             quaternion;
         std::vector<int>                      marker_ids_process, marker_ids_process_ordered;
         std::vector<double>                   marker_lengths_process, marker_lengths_process_ordered;
@@ -163,6 +167,8 @@ ArucoDetections ArucoMarkerPosition::detectArucosPoses(const sensor_msgs::ImageC
         _current_image = cv_bridge::toCvShare(image_msg, "bgr8")->image;
         output_image   = _current_image.clone();
 
+        // aruco function to detect markers, it returns the four corners of the marker, the ids, and the rejected
+        // candidates
         cv::aruco::detectMarkers(
                 _current_image,
                 _aruco_dictionary,
@@ -171,34 +177,47 @@ ArucoDetections ArucoMarkerPosition::detectArucosPoses(const sensor_msgs::ImageC
                 _aruco_detector_parameters,
                 rejected_candidates);
 
-        for (int i = 0; i < _marker_ids.size(); i++) {
+        for (int i = 0; i < _marker_ids.size();
+             i++) { // this loop checks if the detected markers are stored in the markers_configuration.yaml file
             auto it = std::find(_store_marker_ids.begin(), _store_marker_ids.end(), _marker_ids[i]);
 
-            if (it != _store_marker_ids.end()) {
-                int position = std::distance(_store_marker_ids.begin(), it);
+            if (it != _store_marker_ids.end()) { // if the marker is stored, then it is going to be process
+                int position = std::distance(_store_marker_ids.begin(), it); // this gives the position of the marker
+                                                                             // in the markers_configuration.yaml file
 
-                marker_corners_process.push_back(marker_corners[i]);
+                marker_corners_process.push_back(
+                        marker_corners[i]); // store the corners of the marker that are going to be process due to the
+                                            // fact that they are stored in the markers_configuration.yaml file
                 marker_ids_process.push_back(_marker_ids[i]);
                 marker_lengths_process.push_back(_store_marker_lengths[position]);
                 marker_frames_process.push_back(_store_marker_frames[position]);
 
             } else {
-                ROS_WARN("The aruco marker with id %d is not going to be process.", _marker_ids[i]);
+                ROS_WARN(
+                        "The aruco marker with id %d is not going to be process.",
+                        _marker_ids[i]); // if the marker detectec is not stored we will get a warning saying that the
+                                         // marker is not going to be process
             }
         }
 
-        std::vector<size_t> indices(marker_lengths_process.size());
-        std::iota(indices.begin(), indices.end(), 0);
-        std::sort(indices.begin(), indices.end(), [&marker_lengths_process](size_t i1, size_t i2) {
-            return marker_lengths_process[i1] < marker_lengths_process[i2];
+        std::vector<size_t> indices(marker_lengths_process.size()); // creates a vector with indices
+        std::iota(indices.begin(), indices.end(), 0); // fill the vector with values from 0 to the size of the vector
+                                                      // marker_lengths_process
+        std::sort(indices.begin(), indices.end(), [&marker_lengths_process](size_t i1, size_t i2) { // LAMBDA FUNCTION
+            return marker_lengths_process[i1]
+                 < marker_lengths_process[i2]; // sorting the vector indices according to the ascending values so the
+                                               // output would be an indices vector that may be looking like this {3 1 2
+                                               // 0} instead of {0 1 2 3}
+            // if two values are equal, then the order is not changed
         });
 
-        marker_ids_process_ordered.resize(marker_lengths_process.size());
+        marker_ids_process_ordered.resize(marker_lengths_process.size()); // resize new vectors to store the values
         marker_lengths_process_ordered.resize(marker_lengths_process.size());
         marker_frames_process_ordered.resize(marker_lengths_process.size());
         marker_corners_process_ordered.resize(marker_lengths_process.size());
 
-        for (size_t i = 0; i < marker_lengths_process.size(); i++) {
+        for (size_t i = 0; i < marker_lengths_process.size();
+             i++) { // store the values in the new vectors according to the order of the indices vector
             marker_ids_process_ordered[i]     = marker_ids_process[indices[i]];
             marker_lengths_process_ordered[i] = marker_lengths_process[indices[i]];
             marker_frames_process_ordered[i]  = marker_frames_process[indices[i]];
@@ -207,17 +226,23 @@ ArucoDetections ArucoMarkerPosition::detectArucosPoses(const sensor_msgs::ImageC
         }
 
         for (double num : marker_lengths_process_ordered) {
-            marker_lengths_frequency[num]++;
+            marker_lengths_frequency[num]++; // this loop look for the number of times a value is repeated in the vector
+                                             // marker_lengths_process_ordered the output would look like this
+                                             // {{0.066,3},{0.132,2}} which means that the value 0.066 is repeated 3
+                                             // times and the value 0.132 is repeated 2 times
             ROS_DEBUG_STREAM("Marker lengths frequency value: " << marker_lengths_frequency[num]);
             ROS_DEBUG_STREAM("Marker lengths frequency key: " << num);
             ROS_DEBUG_STREAM("Marker lengths frequency size: " << marker_lengths_frequency.size());
         }
 
-        auto freq_aux = marker_lengths_frequency.begin();
-        for (int i = 0; i < marker_lengths_frequency.size(); i++) {
-            length = freq_aux->first;
-            freq   = freq_aux->second;
+        auto freq_aux = marker_lengths_frequency.begin(); // iterator that points to the first element of the map
+        for (int i = 0; i < marker_lengths_frequency.size();
+             i++) {                    // if there are 2 different lengths this would be repeated 2 times
+            length = freq_aux->first;  // length of the marker
+            freq   = freq_aux->second; // number of times the length is repeated
 
+            // we just want to process that have the length that is treated in this iteration so we create a vector with
+            // the corners of the markers that have the same length
             marker_corners_input.clear();
             for (int i = vector_position; i < (vector_position + freq); i++) {
                 marker_corners_input.push_back(marker_corners_process_ordered[i]);
@@ -226,6 +251,7 @@ ArucoDetections ArucoMarkerPosition::detectArucosPoses(const sensor_msgs::ImageC
             ROS_DEBUG_STREAM("length: " << length);
             ROS_DEBUG_STREAM("freq: " << freq);
 
+            // this function estimates the pose of the marker, it returns the rotation and translation vectors
             cv::aruco::estimatePoseSingleMarkers(
                     marker_corners_input,
                     length,
@@ -235,7 +261,9 @@ ArucoDetections ArucoMarkerPosition::detectArucosPoses(const sensor_msgs::ImageC
                     translation_vecs,
                     _obj_points);
 
-            for (int i = 0; i < rotation_vecs.size(); i++) {
+            for (int i = 0; i < rotation_vecs.size();
+                 i++) { // the rotation vector has 3 values, so we need to
+                        // transform it to a quaternion and added to the quaternion_vecs_out
                 rotation_vector = rotation_vecs[i];
                 theta           = cv::norm(rotation_vector);
                 s               = sin(theta / 2);
@@ -246,14 +274,17 @@ ArucoDetections ArucoMarkerPosition::detectArucosPoses(const sensor_msgs::ImageC
                 quaternion[0]   = cos(theta / 2);
                 rotation_vecs_draw.push_back(rotation_vecs[i]);
                 quaternion_vecs_out.push_back(quaternion);
-                translation_vecs_out.push_back(translation_vecs[i]);
+                translation_vecs_out.push_back(
+                        translation_vecs[i]); // the traslation vector is directly added to the translation_vecs_out
             }
 
-            vector_position = vector_position + freq;
+            vector_position = vector_position + freq; // this is to know the position of the next marker that is going
+                                                      // to be process
             freq_aux++;
             ROS_DEBUG_STREAM("vector_position: " << vector_position);
         }
 
+        // VISUALIZATION
         if (_log_level == ros::console::levels::Debug) {
             for (int i = 0; i < marker_ids_process.size(); i++) {
                 cv::aruco::drawDetectedMarkers(output_image, marker_corners, _marker_ids);
@@ -280,7 +311,7 @@ ArucoDetections ArucoMarkerPosition::detectArucosPoses(const sensor_msgs::ImageC
             ROS_DEBUG_STREAM("Quaternion: " << quaternion_vecs_out[i]);
         }
 
-        ArucoDetections aruco_detections;
+        ArucoDetections aruco_detections; // this is the output of the function that is a structure
         aruco_detections.rvecs  = quaternion_vecs_out;
         aruco_detections.tvecs  = translation_vecs_out;
         aruco_detections.ids    = marker_ids_process;
@@ -295,7 +326,7 @@ ArucoDetections ArucoMarkerPosition::detectArucosPoses(const sensor_msgs::ImageC
 }
 
 void ArucoMarkerPosition::publishDetections(const std_msgs::Header& header, ArucoDetections aruco_detections)
-{
+{ // this function publishes the detections in a custom message called MarkerDetectionArray
     ROS_DEBUG("[%s]", __PRETTY_FUNCTION__);
     ROS_DEBUG_STREAM("Ids size: " << aruco_detections.ids.size());
 
